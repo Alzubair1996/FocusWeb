4eab4b330e53fa991ec188d048d84bd4da0ab3c9
Alzubair Faiz <Alzubair1996@gmail.com>
2023-11-25 15:14:00 +0400

    ddv

---
+++ lib/Location_ofEvent.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart'; // Required for using Colors
+
+
+
+
+class Location_ofEvents {
+  final String name;
+  final String location;
+  final String Latitude;
+  final String longitude;
+// Define the color property with late initialization
+
+  Location_ofEvents(
+      this.name,
+      this.location,
+      this.Latitude,
+      this.longitude,
+      );
+ // Assign the provided color to the field
+
+
+// Other methods or constructors if needed
+}
